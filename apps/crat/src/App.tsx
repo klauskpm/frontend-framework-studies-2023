bc18f44f696e4eb742865f0630ea11b9dfe26e10
Klaus Kazlauskas <klausmachado@gmail.com>
2023-03-29 22:19:58 -0400

    refactor(crat): move login page to its own component

--- apps/crat/src/App.tsx
+++ apps/crat/src/App.tsx
@@ -1,56 +1,8 @@
 import React from "react";
-import "./App.css";
+import LoginPage from "./LoginPage";
 
 function App() {
-  return (
-    <div className="flex min-h-full items-center justify-center px-4 py-20 sm:px-6 lg:px-8">
-      <div className="bg-base-100 w-full max-w-md space-y-2 rounded p-6 shadow-md">
-        <div className="prose">
-          <h2 className="text-center">Sign in to your account</h2>
-        </div>
-        <form
-          className="mx-12 mt-8 flex flex-col items-center space-y-4"
-          action="#"
-          method="POST"
-        >
-          <label htmlFor="email-address" className="sr-only">
-            Email address
-          </label>
-          <input
-            id="email-address"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className="input input-bordered bg-content w-full max-w-xs"
-            placeholder="Email address"
-          />
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-            className="input input-bordered w-full max-w-xs"
-            placeholder="Password"
-          />
-
-          <div className="flex w-full flex-row justify-between pt-4">
-            <button type="submit" className="btn btn-outline">
-              Register
-            </button>
-
-            <button type="submit" className="btn btn-primary">
-              Sign in
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
---
+++ apps/crat/src/LoginPage.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import LoginPage from "./LoginPage";
+
+describe("LoginPage", () => {
+  it("should render the login page", () => {
+    render(<LoginPage />);
+    const headerElement = screen.getByText(/sign in to your account/i);
+    const emailInput = screen.getByPlaceholderText(/email address/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const registerButton = screen.getByRole("button", { name: /register/i });
+    const signInButton = screen.getByRole("button", { name: /sign in/i });
+
+    expect(headerElement).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+    expect(signInButton).toBeInTheDocument();
+  });
+});
