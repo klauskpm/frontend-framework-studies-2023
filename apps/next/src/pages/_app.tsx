5de21b63dbeeb6806509639bdc58fc6ec7abf8b6
Klaus Kazlauskas <klausmachado@gmail.com>
2023-04-02 16:17:57 -0400

    feat(next): add package

--- apps/next/src/pages/_app.tsx
+++ apps/next/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { foo, bar, MyComponent } from "@shared/simple";
+
+console.log(foo);
+bar();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MyComponent />
+      <Component {...pageProps} />
+    </>
+  );
 }
