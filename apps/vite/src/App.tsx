f2b0192696b8a4a0f75ae8f4ad0d87acec0bf98a
Klaus Kazlauskas <klausmachado@gmail.com>
2023-04-16 21:16:41 -0400

    feat(vite): create skeleton account page

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { LoginPage } from "@shared/simple";
-import { getUser, registerUser } from "./auth";
+import { registerUser } from "./auth";
 import { Country, getCountries } from "./countries";
+import { supabase } from "./supabaseClient";
+import Account from "./Account";
+
 
 function App() {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [session, setSession] = useState<any>(null);
 
   useEffect(() => {
     getCountries().then(({ data }) => {
@@ -14,10 +18,16 @@ function App() {
   }, []);
 
   useEffect(() => {
-    getUser().then((response) => {
-      console.log(response);
-    });
-  }, []);
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      console.log("getSession", session)
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log("onAuthStateChange", session)
+      setSession(session)
+    })
+  }, [])
 
   function onSubmit(formData: any) {
     console.log("onSubmit", formData);
@@ -32,7 +42,7 @@ function App() {
 
   return (
     <>
-      <LoginPage onSubmit={onSubmit} />
+      {!session ? <LoginPage onSubmit={onSubmit} /> : <Account session={session} />}
       <ul>
         {countries.map((country) => (
           <li key={country.name}>{country.name}</li>
