489a17ccaea3a68425ef65a34c86524e7d04de3f
Klaus Kazlauskas <klausmachado@gmail.com>
2023-04-20 22:31:52 -0400

    feat(vite): change home page to look more like a home page

--- apps/vite/src/pages/index.tsx
+++ apps/vite/src/pages/index.tsx
@@ -18,9 +18,8 @@ function Home() {
       <div className="hero h-full bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="text-5xl font-bold">Welcome \o/</h1>
+            <p className="py-6">Hiiii! Enjoy your stay</p>
           </div>
         </div>
       </div>
