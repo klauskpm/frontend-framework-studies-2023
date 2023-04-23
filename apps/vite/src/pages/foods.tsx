aa1d3ee10815a9ed1027d864c841bd1864c92ade
Klaus Kazlauskas <klausmachado@gmail.com>
2023-04-23 08:13:13 -0400

    feat(vite): fetch and list foods

--- apps/vite/src/pages/foods.tsx
+++ apps/vite/src/pages/foods.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import { Database } from "../../supabase";
+
+export type Food = Database["public"]["Tables"]["foods"]["Row"];
+
+export async function getFoods() {
+  return supabase.from("foods").select();
+}
+
 export default function Foods() {
-    return <div className="prose">
-        <h1>Foods</h1>
-    </div>;
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    getFoods().then(({ data }) => {
+      if (!data) return;
+      setFoods(data);
+    });
+  }, []);
+
+  return (
+    <div className="prose">
+      <h1>Foods</h1>
+      <ul>
+        {foods.map((food: any) => (
+          <li key={food.id}>{food.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- apps/vite/supabase.ts
+++ apps/vite/supabase.ts
@@ -23,12 +23,73 @@ export interface Database {
           name?: string
         }
       }
+      foods: {
+        Row: {
+          created_at: string | null
+          description: string | null
+          id: number
+          image_url: string | null
+          title: string
+        }
+        Insert: {
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          image_url?: string | null
+          title: string
+        }
+        Update: {
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          image_url?: string | null
+          title?: string
+        }
+      }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          full_name: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+          website: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+      }
     }
     Views: {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      delete_avatar: {
+        Args: {
+          avatar_url: string
+        }
+        Returns: Record<string, unknown>
+      }
+      delete_storage_object: {
+        Args: {
+          bucket: string
+          object: string
+        }
+        Returns: Record<string, unknown>
+      }
     }
     Enums: {
       [_ in never]: never
