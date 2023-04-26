1be510b19898a39ec1a04be75a2c579e03366629
Klaus Kazlauskas <klausmachado@gmail.com>
2023-04-25 21:38:35 -0400

    feat(vite): show foods on table

--- apps/vite/src/pages/foods/index.tsx
+++ apps/vite/src/pages/foods/index.tsx
@@ -5,15 +5,12 @@ import { Link } from "react-router-dom";
 
 export type Food = Database["public"]["Tables"]["foods"]["Row"];
 
-export async function getFoods() {
+async function getFoods() {
   return supabase.from("foods").select();
 }
 
-export async function deleteFood(id: number) {
-  const { data, error } = await supabase
-    .from('foods')
-    .delete()
-    .eq('id', id);
+async function deleteFood(id: number) {
+  const { data, error } = await supabase.from("foods").delete().eq("id", id);
 
   if (error) {
     throw error;
@@ -29,7 +26,7 @@ export default function Foods() {
     deleteFood(id).then(() => {
       setFoods(foods.filter((food) => food.id !== id));
     });
-  }
+  };
 
   useEffect(() => {
     getFoods().then(({ data }) => {
@@ -39,37 +36,55 @@ export default function Foods() {
   }, []);
 
   return (
-    <div className="prose">
+    <div>
       <h1>Foods</h1>
       <Link to={"/foods/create"} className="link-primary link" role="button">
         Create new food
       </Link>
-      <ul className="list-none">
-        {foods.map((food: any) => (
-          <li key={food.id} className="space-x-3 justify-items-center">
-            <Link to={`/foods/${food.id}`} className="link-primary link">
-              {food.title}
-            </Link>
-            <button className="btn-error btn btn-sm" onClick={() => handleClickDelete(food.id)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-              Delete food
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="w-full overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {foods.map((food: any) => (
+              <tr>
+                <td>
+                  <Link to={`/foods/${food.id}`} className="link-primary link">
+                    {food.title}
+                  </Link>
+                </td>
+                <td>
+                  <div className="dropdown">
+                    <button className="btn-accent btn-sm btn">
+                      Actions...
+                    </button>
+                    <ul className="dropdown-content menu rounded-box w-52 bg-base-300 p-2 shadow">
+                      <li>
+                        <button
+                          className="btn-link btn"
+                          onClick={() => handleClickDelete(food.id)}
+                        >
+                          Delete
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Title</td>
+              <td></td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 }
