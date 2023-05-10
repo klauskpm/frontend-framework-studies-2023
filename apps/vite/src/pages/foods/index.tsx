422f2231d047b2d1a0a26d624f950d1d228aba40
Klaus Kazlauskas <klausmachado@gmail.com>
2023-05-10 17:20:01 -0400

    feat(vite): add pagination element

--- apps/vite/src/pages/foods/index.tsx
+++ apps/vite/src/pages/foods/index.tsx
@@ -173,6 +173,12 @@ export default function Foods() {
                 </tr>
               </tfoot>
             </table>
+            <div className="btn-group">
+              <button className="btn btn-active">1</button>
+              <button className="btn">2</button>
+              <button className="btn">3</button>
+              <button className="btn">4</button>
+            </div>
           </div>
         </Tabs.Content>
         <Tabs.Content
