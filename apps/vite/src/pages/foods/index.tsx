a28f18547c81135c4a5abff8163b92a8760091ff
Klaus Kazlauskas <klausmachado@gmail.com>
2023-05-21 15:25:55 -0400

    refactor(vite): transform foods table into its own component

--- apps/vite/src/pages/foods/index.tsx
+++ apps/vite/src/pages/foods/index.tsx
@@ -134,68 +134,14 @@ export default function Foods() {
           className="grow p-5 rounded-b-md outline-none"
           value="table"
         >
-          <div>Total: {count}</div>
-          <div className="w-full">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Price</th>
-                  <th>Available quantity</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {foods.map((food: any) => (
-                  <tr key={food.id}>
-                    <td>
-                      <Link to={`/foods/${food.id}`} className="link-primary link">
-                        {food.title}
-                      </Link>
-                    </td>
-                    <td>{food.price}</td>
-                    <td>{food.quantity}</td>
-                    <td>
-                      <button
-                        className="btn-error btn-sm btn"
-                        onClick={() => handleClickDelete(food.id)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="h-6 w-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                          />
-                        </svg>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th>Title</th>
-                  <th>Price</th>
-                  <th>Available quantity</th>
-                  <th></th>
-                </tr>
-              </tfoot>
-            </table>
-            <Pagination
-              count={count}
-              currentPage={page}
-              itemsPerPage={itemsPerPage}
-              onClick={handlePageChange}
-            />
-          </div>
+          <Table
+            foods={foods}
+            count={count}
+            page={page}
+            itemsPerPage={itemsPerPage}
+            handlePageChange={handlePageChange}
+            handleClickDelete={handleClickDelete}
+          />
         </Tabs.Content>
         <Tabs.Content
           className="grow p-5 rounded-b-md outline-none"
@@ -208,6 +154,78 @@ export default function Foods() {
     </div>
   );
 }
+
+function Table(props: any) {
+  const { foods, count, page, itemsPerPage, handlePageChange, handleClickDelete } = props;
+
+  return (
+    <>
+      <div>Total: {count}</div>
+      <div className="w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Available quantity</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {foods.map((food: any) => (
+              <tr key={food.id}>
+                <td>
+                  <Link to={`/foods/${food.id}`} className="link-primary link">
+                    {food.title}
+                  </Link>
+                </td>
+                <td>{food.price}</td>
+                <td>{food.quantity}</td>
+                <td>
+                  <button
+                    className="btn-error btn-sm btn"
+                    onClick={() => handleClickDelete(food.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Available quantity</th>
+              <th></th>
+            </tr>
+          </tfoot>
+        </table>
+        <Pagination
+          count={count}
+          currentPage={page}
+          itemsPerPage={itemsPerPage}
+          onClick={handlePageChange}
+        />
+      </div>
+    </>
+  );
+}
+
 function Pagination(props: any) {
   const { count, currentPage, itemsPerPage, onClick } = props;
   const pages = new Array(Math.ceil(count/itemsPerPage)).fill(0);
