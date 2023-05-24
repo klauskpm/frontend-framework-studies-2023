0e835a8aaa65be2b5b99c56f74c6243450995f44
Klaus Kazlauskas <klausmachado@gmail.com>
2023-05-24 09:12:09 -0400

    fix(vite): remove missing countries reference

--- apps/vite/src/countries-data.ts
+++
@@ -1,8 +0,0 @@
-import { Database } from "../supabase";
-import { supabase } from "./supabaseClient";
-
-export type Country = Database["public"]["Tables"]["countries"]["Row"];
-
-export async function getCountries() {
-  return supabase.from("countries").select();
-}
