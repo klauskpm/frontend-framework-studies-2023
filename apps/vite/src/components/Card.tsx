7ac3ff1f76324db6f620761e2624f8ad0731877a
Klaus Kazlauskas <klausmachado@gmail.com>
2023-06-25 17:59:20 -0400

    refactor(vite): remove borders from card

--- apps/vite/src/components/Card.tsx
+++ apps/vite/src/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: any) {
   return (
-    <div className="card max-h-fit w-full border-4 border-accent bg-base-100">
+    <div className="card max-h-fit w-full bg-base-100 shadow-lg shadow-secondary/20">
       {children}
     </div>
   );
