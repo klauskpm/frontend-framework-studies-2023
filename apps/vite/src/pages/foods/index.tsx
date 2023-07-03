0cb998236e9b8fedaa2f7c95de14daea36e1e629
Klaus Kazlauskas <klausmachado@gmail.com>
2023-07-03 19:30:37 -0400

    refactor(vite): make food index page easier to replicate devcycle issue

--- apps/vite/src/pages/foods/index.tsx
+++ apps/vite/src/pages/foods/index.tsx
@@ -1,102 +1,28 @@
-import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
-import * as Tabs from "@radix-ui/react-tabs";
-import { useEffect } from "react";
 import { useVariableValue } from "@devcycle/devcycle-react-sdk";
 
-import { useSession } from "../../SessionProvider";
-import { Card } from "@shared/react-ui";
-
 export default function Foods() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [session] = useSession();
-
+  const canSeeFood = useVariableValue("foods", false);
   const canCreateEditFood = useVariableValue("food-create-edit", false);
   const canSeeFoodList = useVariableValue("food-list", false);
   const canSeeFoodGraph = useVariableValue("food-graph", false);
   const canSeeFoodTable = useVariableValue("food-table", false);
 
-  const defaultTab = "table";
-
-  useEffect(() => {
-    if (location.pathname === "/foods") {
-      navigate(`/foods/${defaultTab}`);
-    }
-  }, [location, navigate]);
-
-  const handleChangeTab = (value: string) => {
-    navigate(`/foods/${value}`);
+  const boolToText = (bool: boolean) => {
+    return bool ? "true" : "false";
   };
 
   return (
     <div className="m-8 w-3/5">
       <div className="mb-4 flex items-end justify-between">
         <h2 className="text-3xl font-bold">Foods</h2>
-        {session?.user && canCreateEditFood && (
-          <Link to={"/foods/create"} className="link-primary link">
-            Add a new food
-          </Link>
-        )}
       </div>
-      <Tabs.Root
-        className="flex flex-col"
-        defaultValue={defaultTab}
-        onValueChange={handleChangeTab}
-      >
-        <Tabs.List className="tabs mb-4" aria-label="Manage your account">
-          {canSeeFoodTable && (
-            <Tabs.Trigger
-              className="tab-bordered tab data-[state=active]:tab-active"
-              value="table"
-            >
-              Table
-            </Tabs.Trigger>
-          )}
-          {canSeeFoodList && (
-            <Tabs.Trigger
-              className="tab-bordered tab data-[state=active]:tab-active"
-              value="list"
-            >
-              List
-            </Tabs.Trigger>
-          )}
-          {canSeeFoodGraph && (
-            <Tabs.Trigger
-              className="tab-bordered tab data-[state=active]:tab-active"
-              value="graph"
-            >
-              Graph
-            </Tabs.Trigger>
-          )}
-        </Tabs.List>
-        {canSeeFoodTable && (
-          <Tabs.Content
-            className="grow rounded-b-md outline-none"
-            value="table"
-          >
-            <Card>
-              <Outlet />
-            </Card>
-          </Tabs.Content>
-        )}
-        {canSeeFoodList && (
-          <Tabs.Content className="grow rounded-b-md outline-none" value="list">
-            <Card>
-              <Outlet />
-            </Card>
-          </Tabs.Content>
-        )}
-        {canSeeFoodGraph && (
-          <Tabs.Content
-            className="grow rounded-b-md outline-none"
-            value="graph"
-          >
-            <Card>
-              <Outlet />
-            </Card>
-          </Tabs.Content>
-        )}
-      </Tabs.Root>
+      <ul>
+        <li>canSeeFood: {boolToText(canSeeFood)}</li>
+        <li>canCreateEditFood: {boolToText(canCreateEditFood)}</li>
+        <li>canSeeFoodList: {boolToText(canSeeFoodList)}</li>
+        <li>canSeeFoodGraph: {boolToText(canSeeFoodGraph)}</li>
+        <li>canSeeFoodTable: {boolToText(canSeeFoodTable)}</li>
+      </ul>
     </div>
   );
 }
