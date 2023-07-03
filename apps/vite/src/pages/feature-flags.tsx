0a7ac71103b85271b485049f7d4cb30bdb81a373
Klaus Kazlauskas <klausmachado@gmail.com>
2023-07-03 19:42:27 -0400

    refactor(vite): change feature flags page to force a single user with AAAA ID

--- apps/vite/src/pages/feature-flags.tsx
+++ apps/vite/src/pages/feature-flags.tsx
@@ -1,13 +1,8 @@
-import { useSession } from "../SessionProvider";
-
 export default function FeatureFlags() {
-  const [session] = useSession();
-  const user = session?.user;
-
   return (
     <iframe
       data-dvc-widget="dvc-iframe"
-      src={`https://opt-in.devcycle.com/?userId=${user?.id}&sdkKey=${
+      src={`https://opt-in.devcycle.com/?userId=AAAA&sdkKey=${
         import.meta.env.VITE_DVC_SDK_KEY
       }`}
       title="Feature Opt-In"
