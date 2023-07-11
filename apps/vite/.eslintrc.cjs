d218e6c4b72c985377ea60fd3515a4d922ed4e92
Klaus Kazlauskas <klausmachado@gmail.com>
2023-07-11 17:34:33 -0400

    feat(vite): install @tanstack/react-query

--- apps/vite/.eslintrc.cjs
+++ apps/vite/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
     "turbo",
     "prettier",
   ],
