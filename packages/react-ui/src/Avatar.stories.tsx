0556f34d7a0840ed2d8ea37ff31a2388c0f3943b
Klaus Kazlauskas <klausmachado@gmail.com>
2023-07-30 19:00:47 -0400

    feat(react-ui): add addon-a11y

--- packages/react-ui/src/Avatar.stories.tsx
+++ packages/react-ui/src/Avatar.stories.tsx
@@ -3,6 +3,7 @@ import Avatar from "./Avatar";
 export default {
   title: "Avatar",
   component: Avatar,
+  tags: ["autodocs"],
   argTypes: {
     size: {
       control: { type: "select", options: ["small", "medium", "big"] },
