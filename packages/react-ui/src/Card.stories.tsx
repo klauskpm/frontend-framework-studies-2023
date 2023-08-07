d91173a31e58df9c827051a0703d015e67e8734a
Klaus Kazlauskas <klausmachado@gmail.com>
2023-08-07 18:47:14 -0400

    feat(react-ui): create stories for card

---
+++ packages/react-ui/src/Card.stories.tsx
@@ -0,0 +1,22 @@
+import Card from "./Card.tsx";
+
+export default {
+  title: "Card",
+  component: Card,
+};
+
+export const JustText = {
+  args: {
+    children: "Just text",
+  },
+};
+
+export const ContentWithPadding = {
+  args: {
+    children: (
+      <div className="p-4">
+        Just text, but a div with padding is surrounding it
+      </div>
+    ),
+  },
+};
