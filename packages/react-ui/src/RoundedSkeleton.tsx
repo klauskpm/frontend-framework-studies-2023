67f31c735197e59943c0c7a9214c81df76c8c876
Klaus Kazlauskas <klausmachado@gmail.com>
2023-08-09 12:47:03 -0400

    Revert "feat(react-ui): delete RoundedSkeleton"
    This reverts commit 844badca70d9824dbc5c4c6b115b090952c027ba.

---
+++ packages/react-ui/src/RoundedSkeleton.tsx
@@ -0,0 +1,4 @@
+export default function RoundedSkeleton({ className }: any) {
+  const cssClasses = `animate-pulse rounded-full bg-base-content ${className}`;
+  return <div className={cssClasses}></div>;
+}
