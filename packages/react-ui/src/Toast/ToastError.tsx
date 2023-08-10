46ed87abb3059ae4a42892348481a8cf089539ed
Klaus Kazlauskas <klausmachado@gmail.com>
2023-08-09 20:16:17 -0400

    fix(react-ui): remove useless div from Toast

--- packages/react-ui/src/Toast/ToastError.tsx
+++ packages/react-ui/src/Toast/ToastError.tsx
@@ -22,31 +22,29 @@ export default function ToastError({
       {...props}
     >
       <div className="alert alert-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 shrink-0 stroke-current"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <div>
-            <Toast.Title asChild>
-              <h3 className="font-bold">Error!</h3>
-            </Toast.Title>
-            <Toast.Description asChild>
-              <div className="text-xs">{message}</div>
-            </Toast.Description>
-          </div>
+          <Toast.Title asChild>
+            <h3 className="font-bold">Error!</h3>
+          </Toast.Title>
+          <Toast.Description asChild>
+            <div className="text-xs">{message}</div>
+          </Toast.Description>
         </div>
         <Toast.Close asChild>
-          <button className="btn-ghost btn">Dismiss</button>
+          <button className="btn btn-ghost">Dismiss</button>
         </Toast.Close>
       </div>
     </Toast.Root>
